5c77dc43192ef975f4badbf02871080cd1ef28c4
Angel <a21310399@ceti.mx>
2022-03-24 01:36:05 -0600

    inicio y fin de tarea

---
+++ SerieFibonacci.cpp
@@ -0,0 +1,34 @@
+//Declaración de librería
+#include <iostream>
+
+using namespace std;
+
+const string INFO = "Este programa imprime la sucesion de fibonacci desde \
+1 hasta N\n";
+
+//Función principal
+int main(){
+
+    //Declaración de variables
+    unsigned long long aux = 1, fib = 0, lim, init;
+
+    //Entrada y lectura de datos
+    cout << INFO << endl;
+    cout << "Ingrese un numero para la sucesion de fibonacci: ";
+    cin >> lim;
+
+    //Condicional de límite
+    if(lim > 0) {
+        //Ciclo para acomodar los numeros de la serie Fibonacci
+        for(init = 1; init <= lim; init++) {
+            cout << fib;
+            aux += fib; /* lo mismo que aux = aux + fib; */
+            fib = aux - fib;
+        }
+    } else {
+        cout << "El numero debe ser mayor a cero!!" << endl;
+    }
+    
+    cout << "\n";
+    return 0;
+}
